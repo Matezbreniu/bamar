8f7c8cdf34c2469a4485767c06a5be7f4a0f7056
Mateusz Bręk <matezbreniu@gmail.com>
2020-07-06 10:02:43 +0200

    Updates

---
+++ precache-manifest.4c89fbe1252deb598902d236c07780ed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c87cf8fcc6e59d181a80567a13301d4",
+    "url": "/bamar/index.html"
+  },
+  {
+    "revision": "7ce74db753e706c126e4",
+    "url": "/bamar/static/css/main.73b13bcb.chunk.css"
+  },
+  {
+    "revision": "a9df78061548da82c478",
+    "url": "/bamar/static/js/2.8f783a3f.chunk.js"
+  },
+  {
+    "revision": "f731224685885338d153242cbfb8e85f",
+    "url": "/bamar/static/js/2.8f783a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ce74db753e706c126e4",
+    "url": "/bamar/static/js/main.213057b2.chunk.js"
+  },
+  {
+    "revision": "aa71630c1cf675a9d486",
+    "url": "/bamar/static/js/runtime-main.88fe4ae5.js"
+  },
+  {
+    "revision": "47dadb8228b76b03b4487979e7ff6daa",
+    "url": "/bamar/static/media/ballet.47dadb82.svg"
+  },
+  {
+    "revision": "c8e3632c253c603a5c697f4687f37c2a",
+    "url": "/bamar/static/media/basketball.c8e3632c.svg"
+  },
+  {
+    "revision": "9eaf9752ab510f58d338d9482c97e85b",
+    "url": "/bamar/static/media/chess.9eaf9752.svg"
+  },
+  {
+    "revision": "b8e6f5eb4f13e0744b3ddce135699a1e",
+    "url": "/bamar/static/media/football.b8e6f5eb.svg"
+  },
+  {
+    "revision": "671d2ae75bdfd6d7d6d3c2e0084da841",
+    "url": "/bamar/static/media/hippies.671d2ae7.svg"
+  },
+  {
+    "revision": "7f7a557bfac82b36769fd6c585df57ee",
+    "url": "/bamar/static/media/karate.7f7a557b.svg"
+  }
+]);
