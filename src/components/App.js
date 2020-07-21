dcd5804fec85600957fb1fbfc639ca59ab6a15ae
Mateusz Bręk <matezbreniu@gmail.com>
2020-07-21 18:34:40 +0200

    added eases to animations

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,15 @@ gsap.registerPlugin(ScrollTrigger);
 class App extends Component {
   state = {};
 
+  setAnimationForHeader = () => {
+    const headerItems = document.querySelectorAll('.headerContainer *');
+    gsap.set(headerItems, {y: 300, opacity: 0});
+    headerItems.forEach((item, index) => {
+      const delay = 0.3 * index;
+      gsap.to(item, {y: 0, opacity: 1, duration: 0.5, delay: delay});
+    });
+  };
+
   setAnimationForTitles = () => {
     const titles = document.querySelectorAll('.title');
     gsap.set(titles, {x: 300, opacity: 0});
@@ -22,8 +31,10 @@ class App extends Component {
           start: 'top 80%',
           end: '+=100px',
         },
+        ease: 'back.out(1.7)',
         x: 0,
         opacity: 1,
+        duration: 1,
       });
     });
   };
@@ -31,15 +42,17 @@ class App extends Component {
   setAnimationForServices = () => {
     const serviceContainers = document.querySelectorAll('.serviceContainer');
     gsap.set(serviceContainers, {opacity: 0, scale: 0});
-    serviceContainers.forEach((serviceContainer, index) => {
+    serviceContainers.forEach((serviceContainer) => {
       gsap.to(serviceContainer, {
         scrollTrigger: {
           trigger: serviceContainer,
           start: 'bottom 80%',
           end: '+=100',
         },
+        ease: 'bounce.out',
         opacity: 1,
         scale: 1,
+        duration: 1,
       });
     });
   };
@@ -83,6 +96,7 @@ class App extends Component {
   };
 
   componentDidMount() {
+    this.setAnimationForHeader();
     this.setAnimationForTitles();
     this.setAnimationForServices();
     this.setAnimationForContact();
